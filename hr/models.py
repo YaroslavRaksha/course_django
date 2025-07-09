1aa72a6c3b2560712b818ae7aa4a7fc841c2bc9c
Yaroslav <rakshayaroslav37@gmail.com>
2025-07-09 15:27:02 +0300

    Commit

--- hr/models.py
+++ hr/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    tax_code = models.CharField(max_length=10)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Company.objects.exists():
+            raise ValidationError('There can be only one Company instance.')
+        return super(Company, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 class Department(models.Model):
     name = models.CharField(max_length=200)
@@ -15,6 +28,7 @@ class Position(models.Model):
     title = models.CharField(max_length=200)
     department = models.ForeignKey('Department', on_delete=models.CASCADE)
     is_manager = models.BooleanField(default=False)
+    job_description = models.TextField(blank=True)
 
     def save(self, *args, **kwargs):
         if self.is_manager:
@@ -31,3 +45,4 @@ class Employee(AbstractUser):
     hire_date = models.DateField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
